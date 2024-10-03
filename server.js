64c6b0a31bfa3c1b26c76b3535b8986369123c5b
NPNKhoa <khoa.nphucnguyen@gmail.com>
2024-10-03 11:42:47 +0700

    Cai dat cac handler truy xuat CSDL

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 const app = require('./app');
 const config = require('./app/config');
+const MongoDB = require('./app/utils/mongodb.util.js');
 
-const PORT = config.app.port;
+async function startServer() {
+  try {
+    await MongoDB.connect(config.db.uri);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+    console.log(`Connected to database!`);
+
+    const PORT = process.env.PORT || 5000;
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.log(`Cannot connect to DB with error: ${error}`);
+    process.exit();
+  }
+}
+
+startServer();
